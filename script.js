253f9dddf2a0873cd0a720f5220af323d07736db
CAROL SOUZA LIMA <00001094315229sp@aluno.educacao.sp.gov.br>
2024-09-13 10:19:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const textInput = document.getElementById('text-input');
+const generateBtn = document.getElementById('generate-Btn');
+const qrcodeContainer = document.getElementById('qrcode');
+
+
+generateBtn.addEventListener('click', () => {
+    const text = textInput.value; 
+    if (text.trim() === '') { 
+        alert('Por favor, insira algum texto ou URL');
+        return;
+    }
+
+  
+    qrcodeContainer.innerHTML = '';
+
+ 
+    const qrcode = new QRCode(qrcodeContainer, {
+        text: text,
+        width: 200,
+        height: 200,
+    });
+});
